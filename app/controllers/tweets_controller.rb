70e45a5a1f737a9c7b23b78ecfb3646f93874725
Lea Oriol <lea.oriol.bc@gmail.com>
2015-01-24 14:27:41 -0500

    tweet created

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,18 @@ class TweetsController < ApplicationController
 		@tweet = Tweet.new
 	end
 
+	def create
+		@tweet = Tweet.create(tweet_params)
+		# the flash is a hash --> key and value
+		flash.now[:success] = "Tweet Created"
+		# render new says to go to a new page instead of default
+		render 'new'
+	end
+
+	private
+
+	# tweet_params requires that the tweet contains content only
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
 end
