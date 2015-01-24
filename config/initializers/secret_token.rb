b35bfce507568e59c65345d735852019d1bc3106
Lea Oriol <lea.oriol.bc@gmail.com>
2015-01-24 13:01:07 -0500

    my first twitter commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bctwitter::Application.config.secret_key_base = '55994d6cfddcd708e6042bbc47e6858e2f346a4f5ef70a0dd96abbb0be5f930f422b85f3af632bfd44fbbf8dd6f5e0f0807912efd0e40d13acd62fbb05e643b5'
