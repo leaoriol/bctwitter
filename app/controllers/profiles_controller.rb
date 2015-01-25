383656d5a2c041d0169dbc15c4424f29aa70993a
Lea Oriol <lea.oriol.bc@gmail.com>
2015-01-25 14:03:21 -0500

    prof pic name and prof name request

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,17 +8,23 @@ class ProfilesController < ApplicationController
   def create
   	@profile = Profile.new(profile_params)
   	@profile.user = current_user
+
   	if @profile.save
   		flash[:success] = "Profile Created!"
-  		redirect_to profile_path
+  		redirect_to profile_path(@profile.profile_name)
   	else
   		flash[:error] = "Please try again"
   		render 'new'
   	end
   end
 
+  #show page for every profile
+  def show
+  	@profile = Profile.where(profile_name: params[:id]).first
+  end
+
   private
   def profile_params
-  	params.require(:profile).permit(:picture)
+  	params.require(:profile).permit(:picture, :profile_name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125180320) do
+ActiveRecord::Schema.define(version: 20150125183858) do
 
   create_table "profiles", force: true do |t|
     t.string   "picture"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "profile_name"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
