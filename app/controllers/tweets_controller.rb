950b09646cca2b5c40ce697790f58c18182c2b98
Lea Oriol <lea.oriol.bc@gmail.com>
2015-01-25 12:59:24 -0500

    news feed created

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,16 +4,25 @@ class TweetsController < ApplicationController
 	
 	def new
 		@tweet = Tweet.new
+		@tweets = current_user.tweets
 	end
 
 	def create
 		@tweet = Tweet.new(tweet_params)
 		@tweet.user = current_user
 		@tweet.save
+
+		@tweets = current_user.tweets
+
 		# the flash is a hash --> key and value
 		flash.now[:success] = "Tweet Created"
 		# render new says to go to a new page instead of default
 		render 'new'
+	end
+
+	def index
+		#this defines a news feed
+		@tweets = Tweet.all.reject{ |tweet| tweet.user == current_user}
 	end
 
 	private
